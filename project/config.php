570d7b88cdc9fd4976d6ca9dc4152ffecaabc3df
keith <>
2007-06-17 20:29:14 +0000

    [project @ 39] Pulled in some random useful boilerplate from FugitWiki and deleted the deprecated code.

--- project/config.php
+++ project/config.php
@@ -1,4 +1,11 @@
 <?php
+define('DB_HOST', 'localhost');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+define('APP_VERSION', '0.0.0');
+
 function routes() {
 	$r = new AFK_Routes();
 	$r->route('/', array('_handler' => 'Root'));
@@ -7,8 +14,21 @@ function routes() {
 }
 
 function init() {
+	global $db, $db_logger;
+
 	error_reporting(E_ALL);
 	date_default_timezone_set('UTC');
+	AFK::load_helper('core');
+
+	if (defined('DB_NAME') && DB_NAME != '') {
+		$db = new DB_MySQL(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		$db->set_logger($db_logger = new DB_BasicLogger());
+		new AFK_Session_DB($db, 'sessions');
+		AFK_Cache::set_backend(new Cache_DB($db, 'cache'));
+	}
+
+	session_start();
+
 	return array();
 }
 ?>
--- project/dispatch.php
+++ project/dispatch.php
@@ -8,6 +8,8 @@ AFK::add_class_path(APP_ROOT . '/lib/classes');
 AFK::add_class_path(APP_ROOT . '/classes');
 AFK::add_class_path(APP_ROOT . '/handlers');
 
+AFK::add_helper_path(APP_ROOT . '/lib/helpers');
+
 include(APP_ROOT . '/config.php');
 if (file_exists(APP_ROOT . '/lib/lib.php')) {
 	include(APP_ROOT . '/lib/lib.php');
