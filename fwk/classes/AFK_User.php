b169a320da572f45c6ee9b84696f7303cc1e9645
keith <>
2007-07-22 13:44:39 +0000

    [project @ 78] Another change to the AFK_User world order: the class has been split into a basic implementation (AFK_User) and a Factory/Registry class (AFK_Users). This is a lot less messy and should be a lot more resilient to possible changes in how PHP handles static methods.

--- fwk/classes/AFK_User.php
+++ fwk/classes/AFK_User.php
@@ -1,14 +1,11 @@
 <?php
-abstract class AFK_User {
-
-	private static $impl = null;
-	private static $instances = array();
+class AFK_User {
 
 	private $id;
 	private $username;
 	private $caps = array();
 
-	protected function __construct($id, $username, $caps=array()) {
+	public function __construct($id, $username, $caps=array()) {
 		$this->id = $id;
 		$this->username = $username;
 		$this->add_capabilities($caps);
@@ -38,62 +35,6 @@ abstract class AFK_User {
 		$reqs = func_get_args();
 		$reqs = array_unique($reqs);
 		return count($reqs) == count(array_intersect($this->caps, $reqs));
-	}
-
-	public static function prerequisites() {
-		$reqs = func_get_args();
-		$user = self::get_logged_in_user();
-		if (!call_user_func_array(array($user, 'can'), $reqs)) {
-			call_user_func(array(self::$impl, 'require_auth'));
-		}
-	}
-
-	public static function require_auth() {
-		throw new AFK_HttpException('You lack the required credentials.', 403);
-	}
-
-	public static function get_logged_in_user() {
-		$id = call_user_func(array(self::$impl, 'get_logged_in_user_id'));
-		return self::get($id);
-	}
-
-	protected static function has($id) {
-		return isset(self::$instances[$id]);
-	}
-
-	protected static function add_instance(AFK_User $inst) {
-		self::$instances[$inst->get_id()] = $inst;
-	}
-
-	public static function get($id) {
-		if (is_null(self::$impl)) {
-			throw new AFK_Exception("No implementation for AFK_User specified.");
-		}
-		if (!self::has($id)) {
-			$users = call_user_func(array(self::$impl, 'load'), array($id));
-			if (!isset(self::$instances[$id])) {
-				throw new AFK_Exception("Bad User ID: $id");
-			}
-		}
-		return self::$instances[$id];
-	}
-
-	public static function preload($users) {
-		if (is_null(self::$impl)) {
-			throw new AFK_Exception("No implementation for AFK_User specified.");
-		}
-		$to_load = array_diff($users, array_keys(self::$instances));
-		if (count($to_load) > 0) {
-			call_user_func(array(self::$impl, 'load'), $to_load);
-		}
-	}
-
-	public static function set_implementation($cls) {
-		if (is_string($cls) && is_subclass_of($cls, __CLASS__)) {
-			self::$impl = $cls;
-		} else {
-			throw AFK_Exception("$cls is not an implementation of " . __CLASS__);
-		}
 	}
 }
 ?>
---
+++ fwk/classes/AFK_Users.php
@@ -0,0 +1,72 @@
+<?php
+abstract class AFK_Users {
+
+	private static $impl = null;
+	private $instances = array();
+
+	public static function set_implementation(AFK_Users $impl) {
+		self::$impl = $impl;
+	}
+
+	/* Loading users. */
+
+	protected function add(AFK_User $inst) {
+		$this->instances[$inst->get_id()] = $inst;
+	}
+
+	public static function preload($users) {
+		self::$impl->internal_preload($users);
+	}
+
+	private function internal_preload($users) {
+		$to_load = array_diff($users, array_keys($this->instances));
+		if (count($to_load) > 0) {
+			$this->load($to_load);
+		}
+	}
+
+	protected abstract function load($users);
+
+	/* Fetching users. */
+
+	protected function has($id) {
+		return isset($this->instances[$id]);
+	}
+
+	private function internal_get($id) {
+		if (!$this->has($id)) {
+			$this->load(array($id));
+			if (!$this->has($id)) {
+				throw new AFK_Exception("Bad User ID: $id");
+			}
+		}
+		return $this->instances[$id];
+	}
+
+	protected function get_current_user_id() {
+		return 1;
+	}
+
+	public static function current() {
+		return self::$impl->internal_get(self::$impl->get_current_user_id());
+	}
+
+	public static function get($id) {
+		return self::$impl->internal_get($id);
+	}
+
+	/* Access control. */
+
+	public static function prerequisites() {
+		$reqs = func_get_args();
+		$user = self::current();
+		if (!call_user_func_array(array($user, 'can'), $reqs)) {
+			self::$impl->require_auth();
+		}
+	}
+
+	protected function require_auth() {
+		throw new AFK_HttpException('You lack the required credentials.', 403);
+	}
+}
+?>
