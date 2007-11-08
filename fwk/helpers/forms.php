a365cba41066ce2e9c0c3f9e14b34ecce0da05f1
Keith Gaughan <keith@talideon.com>
2007-11-08 19:32:18 +0000

    select_box() can now deal with 0- and 1-element arrays cleanly.

--- fwk/helpers/forms.php
+++ fwk/helpers/forms.php
@@ -1,13 +1,23 @@
 <?php
 function select_box($name, array $elements, $default=null) {
 	$default = coalesce($default, AFK_Registry::context()->__get($name), null);
-	echo '<select name="', e($name), '" id="', e($name), '">';
-	foreach ($elements as $k=>$v) {
-		echo '<option';
-		if ($k == $default) {
-			echo ' selected="selected"';
+	if (count($elements) > 1) {
+		echo '<select name="', e($name), '" id="', e($name), '">';
+		foreach ($elements as $k => $v) {
+			echo '<option';
+			if ($k == $default) {
+				echo ' selected="selected"';
+			}
+			echo ' value="', e($k), '">', e($v), '</option>';
 		}
-		echo ' value="', e($k), '">', e($v), '</option>';
+		echo '</select>';
+	} elseif (count($elements) == 1) {
+		// This is in a loop so we can be sure what the array key is.
+		// If there's only one, it's selected by default.
+		foreach ($element as $k => $v) {
+			echo '<input type="hidden" name="', e($name), '" value="', e($k), '">', e($v);
+		}
+	} else {
+		echo '&mdash;';
 	}
-	echo '</select>';
 }
