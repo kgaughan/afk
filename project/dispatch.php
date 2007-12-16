34cc71118221e91a4fceed1a5c2abb0fc645988d
Keith Gaughan <keith@talideon.com>
2007-12-16 21:46:51 +0000

    Project is now created by a shell script.

--- project/dispatch.php
+++
@@ -1,3 +0,0 @@
-<?php
-require(dirname(__FILE__) . '/bootstrap.php');
-AFK::process_request(AFK_Registry::routes(), $filters);
--- project/handlers/RootHandler.php
+++
@@ -1,6 +0,0 @@
-<?php
-class RootHandler extends AFK_HandlerBase {
-
-	public function on_get(AFK_Context $ctx) {
-	}
-}
