0fa4c5a0f770461a9ad6fae4fd7e03450cb208fa
Keith Gaughan <keith@talideon.com>
2008-05-15 01:25:48 +0100

    Added AFK_ForceHttpsFilter, which forces all requests to be over HTTPS.

---
+++ fwk/classes/AFK/ForceHttpsFilter.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * AFK - A minimalist PHP web development library.
+ * Copyright (c) Keith Gaughan, 2007. All Rights Reserved.
+ *
+ * For the full copyright and licence terms, please view the LICENCE file
+ * that was distributed with this source code.
+ */
+
+/**
+ * Requires that all requests are done over HTTPS.
+ */
+class AFK_ForceHttpsFilter implements AFK_Filter {
+
+	private $port;
+
+	/**
+	 * @param  port  HTTPS port to use, 443 by default.
+	 */
+	public function __construct($port=443) {
+		$this->port = $port;
+	}
+
+	public function execute(AFK_Pipeline $pipe, $ctx) {
+		if ($ctx->is_secure()) {
+			$pipe->do_next($ctx);
+		} else {
+			$uri = 'https://' . $ctx->HTTP_HOST;
+			if ($this->port != 443) {
+				$uri .= ':' . $this->port;
+			}
+			$uri .= $ctx->REQUEST_URI;
+			$ctx->permanent_redirect($uri);
+		}
+	}
+}
