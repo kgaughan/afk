cb6c55a42e6cfdffbcdd0e9c2fa9fa5403ed82b0
Keith Gaughan <keith@talideon.com>
2008-12-29 04:47:15 +0000

    Removed some vestigial code from the events helper.

--- fwk/helpers/events.php
+++ fwk/helpers/events.php
@@ -1,10 +1,6 @@
 <?php
-function trigger_event($event, $value, $with_universal_observers=false) {
-	return AFK_Registry::_('broker')->trigger($event, $value, $with_universal_observers);
-}
-
-function announce_event($event, $value) {
-	AFK_Registry::_('broker')->announce($event, $value);
+function trigger_event($event, $value) {
+	return AFK_Registry::_('broker')->trigger($event, $value);
 }
 
 function register_listener($event, $callback, $singleton=false) {
