85a7f30346088c7d57e7b6424d777686efc8e952
Keith Gaughan <keith@talideon.com>
2009-02-14 17:41:33 +0000

    Added a help system.

--- fwk/libsync.sh
+++ fwk/libsync.sh
@@ -21,7 +21,7 @@ dispatch () {
 	shift
 
 	case $cmd in
-		production | staging | test)
+		production | staging | testing)
 		pre_deploy $cmd
 		afk_deploy $cmd
 		;;
@@ -33,8 +33,10 @@ dispatch () {
 		fi
 	esac
 
-	echo "Unknown command: '$cmd'"
-	echo -n "Usage: $0 {production|staging|test"
+	if test "x$cmd" != "x"; then
+		echo "Unknown subcommand: '$cmd'"
+	fi
+	echo -n "Usage: $0 {production|staging|testing"
 	# This regular expression is meant to be portable across implementations
 	# of sed, or at least to those I know and care about such as FreeBSD's
 	# sed, and GNU sed, hence the use of basic regex syntax.
@@ -127,6 +129,22 @@ afk_make_directory_structure () {
 	ln -s $1/assets assets/afk
 }
 
+afk_list_help () {
+	local cmd
+
+	echo "Subcommands with help available:"
+	# This regular expression is meant to be portable across implementations
+	# of sed, or at least to those I know and care about such as FreeBSD's
+	# sed, and GNU sed, hence the use of basic regex syntax.
+	for cmd in `set | sed -n "s/^help_\\([a-x]\{1,\}\\) ().*$/\1/p"`; do 
+		echo "  $cmd"
+	done
+}
+
+afk_help_banner () {
+	echo -e "\033[1m$1\033[0m: $2\n\nUsage: $1 $3"
+}
+
 # == Commands =================================================================
 
 do_create () {
@@ -138,6 +156,10 @@ do_create () {
 
 do_purgesvn () {
 	find . -name .svn -type d -exec rm -rf {} +
+}
+
+help_purgesvn () {
+	afk_help_banner "purgesvn" "Removes any .svn directories in the project."
 }
 
 do_lint () {
@@ -154,6 +176,48 @@ do_lint () {
 	if afk_php_lint $method; then
 		echo All clean!
 	fi
+}
+
+help_lint () {
+	afk_help_banner "lint" "Lints PHP files with 'php -l'." "[-a]"
+	cat <<LEFIN
+
+  The command currently only supports Subversion as a VCS.
+
+Options:
+  -a  Lint all files, regardless of whether the VCS thinks they've
+      changed.
+LEFIN
+}
+
+do_help () {
+	if test "x$1" = "x"; then
+		afk_list_help
+	elif test "`type -t help_$1`" = "function"; then
+		help_$1
+	elif test "`type -t do_$1`" = "function"; then
+		echo "Sorry, no help is currently available for '$1'."
+		afk_list_help
+	else
+		echo "Unknown subcommand: '$1'"
+		afk_list_help
+	fi
+}
+
+help_help () {
+	afk_help_banner "help" "Displays help on the given subcommand." "CMD"
+}
+
+help_production () {
+	afk_help_banner "production" "Deploys the project to its production server(s)."
+}
+
+help_staging () {
+	afk_help_banner "staging" "Deploys the project to its staging server(s)."
+}
+
+help_testing () {
+	afk_help_banner "testing" "Deploys the project to its test server(s)."
 }
 
 # == Hooks ====================================================================
