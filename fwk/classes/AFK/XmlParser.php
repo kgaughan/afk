fb1e40fd4f8692e15f94e2c5152ea37aa367aa40
Keith Gaughan <keith@talideon.com>
2009-03-23 09:59:19 +0000

    Added AFK_XmlParser - an expat wrapper.

---
+++ fwk/classes/AFK/XmlParser.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * AFK - A minimalist PHP web development library.
+ * Copyright (c) Keith Gaughan, 2007. All Rights Reserved.
+ *
+ * For the full copyright and licence terms, please view the LICENCE file
+ * that was distributed with this source code.
+ */
+
+/**
+ * Thrown when there are problems loading classes, obviously.
+ */
+class AFK_XmlParser {
+
+	private $namespaces;
+	private $parser;
+
+	public function __construct() {
+		$this->namespaces = array();
+
+		foreach ($this->get_prefixes() as $namespace_uri => $prefix) {
+			if (!in_array($prefix, $this->namespaces)) {
+				$this->namespaces[$namespace_uri] = $prefix;
+			} else {
+				throw new AFK_XmlParserException(sprintf(
+					"The prefix '%s' is already assigned to a namespace.",
+					$prefix));
+			}
+		}
+
+		$this->parser = xml_parser_create_ns('UTF-8', ' ');
+		xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
+		xml_parser_set_option($this->parser, XML_OPTION_TARGET_ENCODING, 'UTF-8');
+		xml_set_object($this->parser, $this);
+		xml_set_element_handler($this->parser, '_on_start_tag', '_on_end_tag');
+		xml_set_character_data_handler($this->parser, '_on_text');
+	}
+
+	public function __destruct() {
+		xml_parser_free($this->parser);
+		unset($this->parser);
+	}
+
+	private function qualify($tag) {
+		$parts = explode(' ', $tag, 2);
+		if (count($parts) == 2 && array_key_exists($parts[0], $this->namespaces)) {
+			return $this->namespaces[$parts[0]] . ':' . $parts[1];
+		}
+		return $tag;
+	}
+
+	public function parse($data, $is_final=true) {
+		if (!xml_parse($this->parser, $data, $is_final)) {
+			$code = xml_get_error_code($this->parser);
+			throw new AFK_XmlParserException(xml_error_string($code), $code);
+		}
+	}
+
+	private function _on_start_tag($p, $tag, array $attrs) {
+		$qualified_attrs = array();
+		foreach ($attrs as $k => $v) {
+			$qualified_attrs[$this->qualify($k)] = $v;
+		}
+		$this->on_start_tag($this->qualify($tag), $qualified_attrs);
+	}
+
+	private function _on_end_tag($p, $tag) {
+		$this->on_end_tag($this->qualify($tag));
+	}
+
+	private function _on_text($p, $text) {
+		$this->on_text($text);
+	}
+
+	protected function get_prefixes() {
+		return array();
+	}
+
+	protected function on_start_tag($tag, array $attrs) {
+	}
+
+	protected function on_end_tag($tag) {
+	}
+
+	protected function on_text($text) {
+	}
+}
