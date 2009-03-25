17608b5812c417b03816d393e0354f42770184c3
Keith Gaughan <keith@talideon.com>
2009-03-25 19:34:38 +0000

    Added AFK_XmlParser, a thin expat wrapper with sane namespace handling.

---
+++ fwk/classes/AFK/XmlParserException.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * AFK - A minimalist PHP web development library.
+ * Copyright (c) Keith Gaughan, 2007. All Rights Reserved.
+ *
+ * For the full copyright and licence terms, please view the LICENCE file
+ * that was distributed with this source code.
+ */
+
+/**
+ * Thrown when an XML document could not be parsed; not to be confused with
+ * an AFK_ParseException.
+ */
+class AFK_XmlParserException extends AFK_Exception { }
+
