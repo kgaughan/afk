b078b4ccf3615fef23f0dfa7960e7ab177cd140a
Keith Gaughan <keith@talideon.com>
2009-07-31 18:29:14 +0100

    Capabilities and groups schema.

---
+++ docs/capabilities.sql
@@ -0,0 +1,47 @@
+-- The capabilities system is simple enough. The whole thing is additive.
+-- Users can have certain capabilities associated with them; they can also
+-- be listed as members of zero or more groups and groups are collections of
+-- capabilities. So the capabilities a user has are those explictly applied
+-- to them combined with those of the groups they're members of.
+-- 
+-- Of course, this *does* lead to a proliferation of tables for managing the
+-- relations between users, groups and capabilities.
+
+CREATE TABLE capabilities (
+    id          SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    slug        CHAR(24)          NOT NULL,
+    description VARCHAR(255)      NOT NULL,
+
+    PRIMARY KEY (id),
+    UNIQUE INDEX ux_slug (slug)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE groups (
+    id          SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    slug        CHAR(24)          NOT NULL,
+    description VARCHAR(255)      NOT NULL,
+
+    PRIMARY KEY (id),
+    UNIQUE INDEX ux_slug (slug)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE users_groups (
+    user_id  INTEGER UNSIGNED  NOT NULL,
+    group_id SMALLINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (user_id, group_id)
+);
+
+CREATE TABLE groups_capabilities (
+    group_id      SMALLINT UNSIGNED NOT NULL,
+    capability_id SMALLINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (group_id, capability_id)
+);
+
+CREATE TABLE users_capabilities (
+    user_id       INTEGER UNSIGNED  NOT NULL,
+    capability_id SMALLINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (user_id, capability_id)
+);
