9121b921d7630239808e6b0aa0344bdb948176f9
Keith Gaughan <keith@talideon.com>
2010-07-29 16:59:56 +0100

    Iterable iterator!

---
+++ fwk/classes/AFK/JoinedIterator.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * AFK - A minimalist PHP web development library.
+ * Copyright (c) Keith Gaughan, 2010. All Rights Reserved.
+ *
+ * For the full copyright and licence terms, please view the LICENCE file
+ * that was distributed with this source code.
+ */
+
+/**
+ * Allows iteration over a number of different iterable objects in
+ * succession.
+ */
+class AFK_JoinedIterator implements Iterator {
+
+	private $iterators = array();
+
+	public function __construct() {
+		$args = func_get_args();
+		$i = 0;
+		foreach ($args as $a) {
+			if ($a instanceof Iterator) {
+				$this->iterators[] = $a;
+			} elseif ($a instanceof IteratorAggregate) {
+				$this->iterators[] = $a->getIterator();
+			} elseif (is_array($a)) {
+				$this->iterators[] = new ArrayIterator($a);
+			} else {
+				throw AFK_NotIterableException(
+					sprintf("Argument %d is not iterable", $i));
+			}
+			$i++;
+		}
+	}
+
+	public function rewind() {
+		// Do nothing.
+	}
+
+	public function current() {
+		return $this->iterators[0]->current();
+	}
+
+	public function key() {
+		return $this->iterators[0]->key();
+	}
+
+	public function valid() {
+		return count($this->iterators) > 0 && $this->iterators[0]->valid();
+	}
+
+	public function next() {
+		if (count($this->iterators) > 1 && !$this->iterators[0]->valid()) {
+			array_shift($this->iterators);
+		}
+		$this->iterators[0]->next();
+	}
+}
